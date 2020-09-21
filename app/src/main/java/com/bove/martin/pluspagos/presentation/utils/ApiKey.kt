c94a5c831382302f721945891feb1efef5418bcd
Martin Bove <mbove77@gmail.com>
2020-09-21 20:07:33 -0300

    Delete ApiKey.kt

--- app/src/main/java/com/bove/martin/pluspagos/presentation/utils/ApiKey.kt
+++
@@ -1,9 +0,0 @@
-package com.bove.martin.pluspagos.presentation.utils
-
-/**
- * Created by Martín Bove on 21-Sep-20.
- * E-mail: mbove77@gmail.com
- */
-object ApiKey {
-    const val API_KEY = "TEST-131fb6f8-f947-4191-a711-94ed8bb9b96f"
-}
