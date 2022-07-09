93b1b927678dec0e4c87c860448622e647210c7a
mbove77 <mbove77@gmail.com>
2022-07-09 19:19:37 -0300

    Add logging to RetrofitModule

--- app/src/main/java/com/bove/martin/pluspagos/data/di/RetrofitModule.kt
+++ app/src/main/java/com/bove/martin/pluspagos/data/di/RetrofitModule.kt
@@ -1,6 +1,7 @@
 package com.bove.martin.pluspagos.data.di
 
 import com.bove.martin.pluspagos.AppConstants
+import com.bove.martin.pluspagos.BuildConfig
 import com.bove.martin.pluspagos.data.retrofit.AuthInterceptor
 import com.bove.martin.pluspagos.data.retrofit.MercadoPagoServices
 import dagger.Module
@@ -8,9 +9,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -20,6 +23,11 @@ object RetrofitModule {
     @Provides
     fun provideRetrofit(): Retrofit {
         val okHttpClientBuilder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC)
+            okHttpClientBuilder.addInterceptor(interceptor)
+        }
         okHttpClientBuilder.addInterceptor(AuthInterceptor())
 
         return Retrofit.Builder()
